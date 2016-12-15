e5c7ec1edd1604e972361f94bacf70a759f30175
mah-esh <maheswara582s@gmail.com>
2016-12-15 14:04:40 +0530

    Code for ScrollView(layouts)

---
+++ User_Interfaces_ScrollView/src/com/mahesh/scrol/LayoutScrol.java
@@ -0,0 +1,15 @@
+package com.mahesh.scrol;
+
+import com.mahesh.user_interfaces.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutScrol extends Activity {
+ @Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.layout_scrol);
+}
+}
